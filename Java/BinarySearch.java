a8218209b9dd81da44000e3219bb95af2e1dc039
Karthikeya914 <karthikeya.chakravaram@gmail.com>
2024-10-29 17:11:56 +0530

    Create BinarySearch.java

---
+++ Java/BinarySearch.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        while (q > 0) {
+            boolean isfound = false;
+            int x = sc.nextInt();
+            int start = 0;
+            int end = size - 1;
+            while(start <= end) {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] > x) {
+                    end = mid - 1;
+                } else if (arr[mid] < x) {
+                    start = mid + 1;
+                } else {
+                    System.out.println("found " + mid);
+                    isfound = true;
+                    break;
+                }
+            }
+            q--;
+            if (!isfound) {
+                System.out.println("not found");
+            }
+        }
+        sc.close();
+    }
+}
