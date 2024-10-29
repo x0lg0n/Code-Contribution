666e026ae6cedc18649a9ae4b31275ad4a1ee50a
aastik-gg <aastiknayyar13@gmail.com>
2024-10-29 23:59:37 +0530

    Update and rename bubbleSort.java to BubbleSort.java

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -1,31 +1,34 @@
 import java.util.Arrays;
 
-public class bubbleSort {
+public class BubbleSort {
 
     // bubble sort:
       //  Bubble sort is an algorithm where you can sort an array by making pairs and comparing between them and through it the largest elemnt in the array get sorted first.
 
-        // Time complexity: O(n^2) in both best and worst case.
+        // Time complexity: O(n^2) in worst case and O(1) in best case.
 
         // Unique Feature: Large values are always sorted first. It only takes one iteration to detect that a collection is already sorted. The best time complexity for Bubble Sort is O(n).
 
-    public static void bubbleSort(int[] arr){
-        for(int i=0; i<arr.length-1; i++){
-            for(int j=0; j<arr.length-1-i; j++){
-                int temp= arr[j];
-                if(arr[j+1]<arr[j]){
-                    arr[j]= arr[j+1];
-                    arr[j+1]= temp;
+        public static void BubbleSort(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
                 }
             }
+            if (!swapped) break;
         }
-
     }
 
     public static void main(String[] args) {
         int[] a= {1, 5, 6, 8, 10, 4, 12, 3, 0, 1};
 
-        bubbleSort(a);
+        BubbleSort(a);
         for(int i=0; i<a.length;i++){
             System.out.print(a[i]+" ");
         }
