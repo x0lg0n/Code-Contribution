e20fb57743181a1aa9a55e62010435e9a47fe047
BlazePhenom <saxmax230505@gmail.com>
2024-10-31 17:27:32 +0530

    Update Rotate90.java
    changed names of variables start and end to startIndex and endIndex for better understandibility of the code.

--- Java/Rotate90.java
+++ Java/Rotate90.java
@@ -1,28 +1,25 @@
 public class Rotate90 {
     public void solve(int[][] A) {
-        int temp = 0;
         for(int i = 0; i<A.length; i++){
             for(int j = i+1; j<A.length; j++){
-                
-                    temp = A[i][j];
+                    int temp = A[i][j]; //the temp variable has been declared to do the swapping process
                      A[i][j] = A[j][i];
                      A[j][i] = temp;
-                
             }
         }
     
     
         for (int i = 0; i < A.length; i++) {
-            int start = 0;
-            int end = A.length - 1;
+            int startIndex = 0; 
+            int endIndex = A.length - 1;
       
-            while (start < end) {
-                temp = A[i][start];
-                A[i][start] = A[i][end];
-                A[i][end] = temp;
+            while (startIndex < endIndex) {
+                temp = A[i][startIndex];
+                A[i][startIndex] = A[i][endIndex];
+                A[i][endIndex] = temp;
                 
-                start++;
-                end--;
+                startIndex++;
+                endIndex--;
             }
         }
     }
