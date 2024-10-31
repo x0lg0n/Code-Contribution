42d4d7fe91f2c3cd3881ad3409d927ab4117ce89
BlazePhenom <saxmax230505@gmail.com>
2024-10-31 16:47:21 +0530

    Update If_else.js

--- Javascript/If_else.js
+++ Javascript/If_else.js
@@ -1,5 +1,4 @@
 const firstName = "Ashu"; //The firstname and gender of a person is fixed(constant) ie it cannot change so its much better to use the const variable as it then wont allow the variable value to be reassigned.
 const gender = "Male";
-if (gender == "Male") {
-    console.log("Hello, " + firstName);
-}
+if (gender === "Male") {
+    console.log("Hello, " + firstName); } 
