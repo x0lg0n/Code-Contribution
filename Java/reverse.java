0a9bdcb3c78646db7b64e309acc43d7ca7984aee
ashu273k <ashu9472.as@gmail.com>
2024-10-31 15:34:18 +0530

    Add file via upload

---
+++ Java/reverse.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+
+public class reverse {
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        int[][] A = new int[n][m];
+
+        for(int i = 0; i < n ; i++){
+            for(int j = 0 ; j  < m; j++){
+                A[i][j] = in.nextInt();
+            }
+        }
+        Reverse(A);
+        
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+        public static int[][] Reverse(int[][] image)
+        {
+        
+        for (int i = 0 ; i < image.length ; i++) {
+        int s = 0;
+        int e = image[0].length -1;
+        while(s < e) {
+            int temp = image[i][s];
+            image[i][s] = image[i][e];
+            image[i][e] =temp;
+            s++;
+            e--;
+        }
+        }
+        return image;
+    }
+    
+
+}
