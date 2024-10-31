0f3d8112f31baaf2e736c34239e4b5fe8e90f505
BlazePhenom <saxmax230505@gmail.com>
2024-10-31 17:08:36 +0530

    Update reverse.java
    I have tried to make the code  more understandable for a beginner.
    Hope this helps :)

--- Java/reverse.java
+++ Java/reverse.java
@@ -7,8 +7,8 @@ public class reverse {
         
         Scanner in = new Scanner(System.in);
 
-        int n = in.nextInt();
-        int m = in.nextInt();
+        int n = in.nextInt(); //for inputing the number of rows.
+        int m = in.nextInt(); //for inputing the number of columns.
 
         int[][] A = new int[n][m];
 
@@ -17,7 +17,7 @@ public class reverse {
                 A[i][j] = in.nextInt();
             }
         }
-        Reverse(A);
+        Reverse(A); //calling the function for reversing each row of the given matrix.
         
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[0].length; j++) {
@@ -25,19 +25,19 @@ public class reverse {
             }
             System.out.println();
         }
-        in.close();
+        in.close();//closing the scanner object to avoid memory leakage in the program.
     }
         public static int[][] Reverse(int[][] image)
         {
         
         for (int i = 0 ; i < image.length ; i++) {
-        int s = 0;
-        int e = image[0].length -1;
-        while(s < e) {
-            int temp = image[i][s];
+        int s = 0; //s refers to the startindex of each row of the matrix.
+        int e = image[0].length -1;//e refers to the endindex of each row of the matrix.
+        while(s < e) { // applying the algorithm for reversing rows.
+            int temp = image[i][s]; //the temp variable has been declared to help in the swapping process.
             image[i][s] = image[i][e];
             image[i][e] =temp;
-            s++;
+            s++; // incrementing value of s and decrementing value of e otherwise of which the program will show an error.
             e--;
         }
         }
