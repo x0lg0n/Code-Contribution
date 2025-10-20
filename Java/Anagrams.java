29ff216460a9d252b89231774515410cae6d5ad7
Hirushi Nethmini <hirushi_n@epiclanka.net>
2025-10-20 14:21:48 +0530

    Anagrams - Simplified code

--- Java/Anagrams.java
+++ Java/Anagrams.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Program Title: Add a Java solution for checking if two words are anagrams
  * Author: [hirushi1999]
@@ -6,7 +8,6 @@
 
 public class Anagrams {
     static boolean isAnagram(String a, String b) {
-        boolean ctt = true;
         String c = a.toLowerCase();
         String d = b.toLowerCase();
 
@@ -15,34 +16,13 @@ public class Anagrams {
         }
         char[] chars1 = c.toCharArray();
         char[] chars2 = d.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
 
-        for(int i=0; i< chars1.length-1; i++){
-            for(int j=0; j<chars1.length-i-1; j++){
-                if(chars1[j] > chars1[j+ 1]){
-                    char temp = chars1[j];
-                    chars1[j] = chars1[j + 1];
-                    chars1[j + 1] = temp;
-                }
-            }
-        }
-
-        for(int i=0; i< chars2.length-1; i++){
-            for(int j=0; j<chars2.length-i-1; j++){
-                if(chars2[j] > chars2[j+ 1]){
-                    char temp = chars2[j];
-                    chars2[j] = chars2[j + 1];
-                    chars2[j + 1] = temp;
-                }
-            }
-        }
         String cb = new String(chars1);
         String cb2 = new String(chars2);
 
-        if(!cb.equals(cb2)){
-            return false;
-        } else{
-            return true;
-        }
+        return cb.equals(cb2);
     }
 
     public static void main(String[] args) {
